12d98fdf24e1108114b7fae5339bf5f370b6f357
buhron <103539649+buhron@users.noreply.github.com>
2022-07-19 14:25:24 -0400

    Update app.py

--- src/init/app.py
+++ src/init/app.py
@@ -2,8 +2,14 @@
 Offcical Pytbetter Engine
 """
 import os
+import sys
 class winsan:
  """
  WinSan is a functionality class containing parts of the system.
  """
- 
+ class io:
+   write = print
+   read = input
+ class inputter:
+  __stdin_io_write__= sys.stdin
+  
